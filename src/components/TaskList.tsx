51421e2c79ca279a0d3eec354c9d779f7698b698
joaotuliojt <joaotuliosoares@hotmail.com>
2021-10-15 15:19:49 -0300

    :tada: Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,39 @@ interface Task {
   isComplete: boolean;
 }
 
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      let newTask = {
+        id: Math.random(), 
+        title: newTaskTitle, 
+        isComplete: false
+      }
+      setTasks(oldState => [...oldState, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTasks);
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task =>task.id != id))
+
   }
 
   return (
